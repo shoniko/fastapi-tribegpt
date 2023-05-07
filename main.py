f48836a915278f43da83a607b4b4cb104babd909
sert121 <yashrajmore29@gmail.com>
2023-05-08 03:52:30 +0530

    m

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "I'm TribeGPT"}
 
 @app.post("/initialize_form/")
 async def submit_form(form_data: FormData):
