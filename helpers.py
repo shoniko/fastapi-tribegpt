d649972bb2c24219689461638edee4e57c516042
shoniko <oleksandr@shoniko.com>
2023-05-12 23:57:16 +0000

    Adapt prompts

--- helpers.py
+++ helpers.py
@@ -12,6 +12,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
 bsl_loader = BeautifulSoupWebReader()
 
+MAX_PROMPT_SIZE = 8000
+
 def _chat_openai(prompt, system_text="",token_limit=2048):
     """ construct an openai gpt3.5/gpt4 call
 
@@ -27,8 +29,8 @@ def _chat_openai(prompt, system_text="",token_limit=2048):
 
     response = openai.ChatCompletion.create(
             messages=chat_query,    
-            # model="gpt-4-0314",
-            model = "gpt-3.5-turbo",
+            model="gpt-4-0314",
+            # model = "gpt-3.5-turbo",
             temperature=0,
             max_tokens=token_limit,
             top_p=1,
@@ -100,40 +102,16 @@ def generate_roadmap(idea:str):
     Create a three phase PRD and roadmap for the following product: {idea}
     Each phase has its own roadmap without dates. 
     Write for a well-informed, professional audience. Be detailed and lengthy. Think step-by-step. 
-    Return in JSON format, with each phase as a key.
-    """+"""
-    JSON Format example:
-    {
-    "roadmap": {
-        "Phase 1": {
-        "Objective": "Develop a Minimum Viable Product (MVP) for the Predictive Maintenance solution using AI algorithms to analyze sensor data and predict equipment failure.",
-        "Roadmap": [
-            "1.1 Conduct market research to identify target industries and customers for the Predictive Maintenance solution.",
-            "1.2 Define the scope of the MVP, including the types of equipment and sensors to be supported, and the specific AI algorithms to be used for analysis.",
-            "1.3 Assemble a cross-functional team, including data scientists, engineers, and domain experts, to design and develop the MVP.",
-            "1.4 Collect and preprocess historical sensor data from target industries to train and validate the AI algorithms.",
-            "1.5 Develop a user-friendly interface for customers to input their equipment and sensor data, and receive predictions on equipment failure.",
-            "1.6 Implement a secure and scalable cloud-based infrastructure to support the processing and storage of large volumes of sensor data.",
-            "1.7 Test the MVP with a small group of pilot customers, and gather feedback on its performance and usability.",
-            "1.8 Refine the MVP based on customer feedback, and prepare for a wider launch."
-         ]
-        }, 
-    }
-    }
+    Return in a format, embeddable into an HTML <div> tag.
     """
 
 
     system_prompt = 'You are a Product manager with expertise in Machine Learning, Generative AI, and Business.'
 
-    phases_text = _chat_openai(prompt, system_prompt)
-    try:
-        phases = json.loads(phases_text)
-        return phases
-    except:
-        print("Error in loading...")
-        return 'Internal Server Error, invalid JSON from gpt'
+    roadmap_text = _chat_openai(prompt, system_prompt)
+    return roadmap_text
 
-def generate_ideas(company_link:str):
+def generate_ideas(company_link:str, role:str=""):
     """
     Generate ideas for the company based on the company description
 
@@ -144,16 +122,27 @@ def generate_ideas(company_link:str):
     """
     company_description = _scrape_website(company_link)
 
-    old_prompt = '''
-    Given a summary of a business in any industry, provide exactly three suggestions on how  AI could be used to improve various aspects of its operations, tailored specifically for that business. 
-    The AI improvement suggestions should be suitable for a professional audience, such as executives or those reporting to executives.
-    Do not include an introduction or conclusion.
-    Please respond with the 3 ideas in JSON format like this: {"idea_1_title": "IDEA 1 DESCRIPTION", "idea_2_title": "IDEA 2 DESCRIPTION", "idea_3_title": "IDEA 3 DESCRIPTION"}.
-    Here is the provide summary of a specific business, which could be in any industry, for you to analyze and suggest AI improvements:
-    {company_description}
-    '''
-    system_prompt = 'You are a really helpful product manager that understand company needs, and specialize in generative AI, so you can act as a good guide.'
-    ideas = _chat_openai(prompt, system_prompt, 4048 - (len(prompt) + len(system_prompt)))
+    prompt = '''
+    You are an expert in Machine Learning, Generative AI, and Business.
+    Present a list of 3 ways generative AI can transform my business.
+    I work in a {role} role at the company, keep this in mind in your response.
+    It should be written as an informative business presentation, for business professional audience. Do not include an introduction or conclusion.
 
+    Please respond with the 3 ideas in JSON format like this:
+
+    {
+        "idea_1_title": "IDEA 1 DESCRIPTION",
+        "idea_2_title": "IDEA 2 DESCRIPTION",
+        "idea_3_title": "IDEA 3 DESCRIPTION"
+    }
+    
+    The following describes my business:
+
+    '''
+    prompt += company_description
+
+    system_prompt = 'You are a really helpful product manager that understand company needs, and specialize in generative AI, so you can act as a good guide.'
+    ideas = _chat_openai(prompt, system_prompt, MAX_PROMPT_SIZE - (len(prompt) + len(system_prompt)))
+    print(ideas)
     return ideas
 
